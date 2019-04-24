adc36e255fe1e97ac2698559e67034085970b837
Mychal Hall <hallmychal@gmail.com>
2019-04-23 23:20:37 -0500

    actions and reducers, first pass

--- devdesk/src/reducers/index.js
+++ devdesk/src/reducers/index.js
@@ -1,9 +1,134 @@
-import {} from "../actions";
+import {
+  FETCH_DATA_START,
+  FETCH_DATA_SUCCESS,
+  FETCH_DATA_FAIL,
+  ADD_TICKET_START,
+  ADD_TICKET_SUCCESS,
+  ADD_TICKET_FAIL,
+  EDIT_TICKET_START,
+  EDIT_TICKET_SUCCESS,
+  EDIT_TICKET_FAIL,
+  DELETE_TICKET_START,
+  DELETE_TICKET_SUCCESS,
+  DELETE_TICKET_FAIL
+} from "../actions";
 
-const initialState = {};
+const initialState = {
+  errorStatusCode: null,
+  fetchingData: false,
+  isLoggingIn: false,
+  creatingTicket: false,
+  updatingTicket: false,
+  resolvingTicket: false,
+  deletingTicket: false,
+  status: null,
+  error: null,
+  students: [
+    {
+      id: "",
+      name: "",
+      email: "",
+      cohort: "",
+      isStudent: true,
+      openedTickets: []
+    }
+  ],
+  admins: [
+    {
+      id: "",
+      name: "",
+      email: "",
+      isAdmin: true,
+      myTickets: []
+    }
+  ],
+  tickets: [
+    {
+      id: "",
+      title: "",
+      shortDescription: "",
+      description: "",
+      category: [],
+      resolved: false,
+      assigned: false
+    }
+  ]
+};
 
-const reducers = (state = initialState, actions) => {
-  switch (actions.type) {
+const reducers = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_DATA_START:
+      return {
+        ...state,
+        fetchingData: true,
+        error: null
+      };
+    case FETCH_DATA_SUCCESS:
+      return {
+        ...state,
+        fetchingData: false,
+        tickets: action.payload
+      };
+    case FETCH_DATA_FAIL:
+      return {
+        ...state,
+        fetchingData: false,
+        error: action.payload
+      };
+    case ADD_TICKET_START:
+      return {
+        ...state,
+        error: "",
+        creatingTicket: true
+      };
+    case ADD_TICKET_SUCCESS:
+      return {
+        ...state,
+        creatingTicket: false,
+        tickets: action.payload
+      };
+    case ADD_TICKET_FAIL:
+      return {
+        ...state,
+        creatingTicket: false,
+        error: action.payload
+      };
+    case DELETE_TICKET_START:
+      return {
+        ...state,
+        error: "",
+        deletingTicket: true
+      };
+    case DELETE_TICKET_SUCCESS:
+      return {
+        ...state,
+        deletingTicket: false,
+        tickets: action.payload
+      };
+    case DELETE_TICKET_FAIL:
+      return {
+        ...state,
+        deletingTicket: false,
+        error: action.payload
+      };
+    case EDIT_TICKET_START:
+      return {
+        ...state,
+        error: "",
+        updatingTicket: true
+      };
+    case EDIT_TICKET_SUCCESS:
+      return {
+        ...state,
+        updatingTicket: false,
+        tickets: action.payload
+      };
+    case EDIT_TICKET_FAIL:
+      return {
+        ...state,
+        updatingTicket: false,
+        error: action.payload
+      };
     default:
       return state;
   }
