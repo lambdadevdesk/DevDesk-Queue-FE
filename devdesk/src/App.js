7a68697a83e21373c04fa19c7cc80e3bbdbe880c
Mychal Hall <hallmychal@gmail.com>
2019-04-25 22:04:25 -0500

    Working...

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -8,6 +8,7 @@ import PrivateRoute from "./components/PrivateRoute";
 // Testing
 import AdminView from "./views/AdminView";
 import StudentView from "./views/StudentView";
+import EditTicket from "./components/EditTicket";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" component={Login} />
         <PrivateRoute exact path="/student_view" component={StudentView} />
         <PrivateRoute exact path="/admin_view" component={AdminView} />
+        <Route path="edit/:id" render={(props)=> <Component {...props} />
       </Layout>
     </Router>
   );
