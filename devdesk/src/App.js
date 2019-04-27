787d5d09afc51541be81171f89f27aa3b076299d
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-04-26 20:58:55 -0400

    merging conflicts with master

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -4,39 +4,18 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import Login from "./components/Login";
 import PrivateRoute from "./components/PrivateRoute";
-import TicketCard from "./components/Tickets/TicketCard";
-import HomePage from "./components/HomePage";
-<<<<<<< HEAD
-import TicketViews from "./views/TicketViews";
-=======
 
 // Testing
 import AdminView from "./views/AdminView";
 import StudentView from "./views/StudentView";
 
->>>>>>> 59296c0d97e40c65423746bc366a00545141af4b
-import TicketForm from "./components/Tickets/TicketForm";
-
-
 function App() {
   return (
     <Router>
       <Layout>
-        <Route path="/" exact component={HomePage} />
         <Route path="/login" component={Login} />
-        <Route
-          path="/tickets/:id"
-          exact
-          render={props => <TicketCard {...props} />}
-        />
-        <Route path="/new-ticket" component={TicketForm} />
-<<<<<<< HEAD
-        <PrivateRoute exact path="/tickets" component={TicketViews} />
-=======
         <PrivateRoute exact path="/student_view" component={StudentView} />
         <PrivateRoute exact path="/admin_view" component={AdminView} />
-        <Route path="edit/:id" render={props => <EditTicket {...props} />} />
->>>>>>> 59296c0d97e40c65423746bc366a00545141af4b
       </Layout>
     </Router>
   );
