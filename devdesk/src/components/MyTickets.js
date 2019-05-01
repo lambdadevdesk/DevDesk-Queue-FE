ee5c98fe0e7d871ce8483ed80593b1facd910dfd
Mychal Hall <hallmychal@gmail.com>
2019-04-30 20:03:36 -0500

    Hit a wall

--- devdesk/src/components/MyTickets.js
+++ devdesk/src/components/MyTickets.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Link from "react-router-dom";
 import { connect } from "react-redux";
-import TicketViews from "../views/TicketViews";
+
 
 const MyTickets = props => {
   return (
